de12c2bbf52d672dff5c29c174f75b263947ef1a
dnda14 <dndamc@gmail.com>
2025-07-05 16:28:55 -0500

    conexion set

---
+++ cliente.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define PORT 8080
+
+int main()
+{
+    printf("\t\t-------------- TCP CLIENT --------------\n");
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+    
+    int cliente_socket;
+    struct sockaddr_in serv_direccion;
+    char buffer[1024] = {0};
+
+    if ((cliente_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        perror("Error en creacion de socket");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_direccion.sin_family = AF_INET;
+    serv_direccion.sin_port = htons(PORT); // puerto al que se conecta
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_direccion.sin_addr) <= 0) // IP del servidor
+    {
+        perror("direccion invalida");
+        exit(EXIT_FAILURE);
+    }
+    if (connect(cliente_socket,(struct sockaddr *)&serv_direccion, sizeof(serv_direccion)) < 0)
+    {
+        perror("Error en el connect");
+        exit(EXIT_FAILURE);
+    }
+    printf("Conectado al servidor en el puerto %d, escribe mensajes:\n", PORT);
+
+    while (1)
+    {
+        printf("> ");
+        fgets(buffer, sizeof(buffer), stdin);
+        send(cliente_socket,buffer,strlen(buffer),0);
+        recv(cliente_socket, buffer, sizeof(buffer), 0);
+        printf("Eco: %s\n", buffer);
+    }
+    closesocket(cliente_socket);
+    return 0;
+    
+
+}
--- servidor.c
+++ servidor.c
@@ -8,53 +8,55 @@
 int main()
 {
     printf("\t\t-------------- TCP SERVER --------------\n");
-
-    int server_fd, cliente_cocket;
-    struct sockaddr_in address;
-    int addrsz = sizeof(address);
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+    int server_fd, cliente_socket;
+    struct sockaddr_in direccion;
+    int addrsz = sizeof(direccion);
     char buffer[1024] = {0};
 
-    if (server_fd = socket(AF_INET, SOCK_STREAM, 0))
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
     {
         perror("Error en creacion de socket");
         exit(EXIT_FAILURE);
-    }
+    }   
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // escucha cualquier IP
-    address.sin_port = htons(PORT);       // puerto en el que escucha
+    direccion.sin_family = AF_INET;
+    direccion.sin_addr.s_addr = INADDR_ANY; // escucha cualquier IP
+    direccion.sin_port = htons(PORT);       // puerto en el que escucha
 
-    if (bind(server_fd, (struct sockaddr *)&address, addrsz) < 0)
+    if (bind(server_fd, (struct sockaddr *)&direccion, addrsz) < 0)
     {
         perror("Error en el bind");
         exit(EXIT_FAILURE);
     }
-    
+
     if (listen(server_fd, 3) < 0)
     {
         perror("Error en el listen");
         exit(EXIT_FAILURE);
     }
     printf("escuchando puerti %d\n", PORT);
-    if ((cliente_cocket = accept(server_fd, (struct sockaddr *)&address, (struct socklen_t *)&addrsz)) < 0)
+    if ((cliente_socket = accept(server_fd, (struct sockaddr *)&direccion, &addrsz)) < 0)
     {
         perror("Error en el accept");
         exit(EXIT_FAILURE);
     }
-    While(1)
+    while (1)
     {
-        int bytes_recibidos = recv(cliente_cocket, buffer, sizeof(buffer), 0);
+        int bytes_recibidos = recv(cliente_socket, buffer, sizeof(buffer), 0);
         if (bytes_recibidos <= 0)
-            ;
         {
             break;
         }
         printf("mesaje recibido: %s\n", buffer);
-        send(client_socket, buffer, bytesrecibidos, 0);
+        send(cliente_socket, buffer, bytes_recibidos, 0);
         memset(buffer, 0, sizeof(buffer));
     }
-    close(cliente_cocket);
-    close(server_fd);
+
+    closesocket(cliente_socket);
+    closesocket(server_fd);
+
     return 0;
-}
+
 }
